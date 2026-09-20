89b79e938243796bfc13caa0f339ce47db5b12c2
alyssasgable <alyssasgable@gmail.com>
2026-09-20 15:50:45 -0400

    Fix Cal.com embed initialization

--- src/components/CalTechHelp.tsx
+++ src/components/CalTechHelp.tsx
@@ -8,37 +8,74 @@ declare global {
 
 const CalTechHelp = () => {
   useEffect(() => {
+    if (window.Cal) return;
+
     const script = document.createElement("script");
     script.type = "text/javascript";
-    script.src = "https://app.cal.com/embed/embed.js";
-    script.async = true;
+    script.innerHTML = `
+      (function (C, A, L) {
+        let p = function (a, ar) { a.q.push(ar); };
+        let d = C.document;
 
-    script.onload = () => {
-      if (!window.Cal) return;
+        C.Cal = C.Cal || function () {
+          let cal = C.Cal;
+          let ar = arguments;
 
-      window.Cal("init", "tech-help", {
-        origin: "https://app.cal.com",
+          if (!cal.loaded) {
+            cal.ns = {};
+            cal.q = cal.q || [];
+
+            d.head.appendChild(d.createElement("script")).src = A;
+            cal.loaded = true;
+          }
+
+          if (ar[0] === L) {
+            const api = function () {
+              p(api, arguments);
+            };
+
+            const namespace = ar[1];
+
+            api.q = api.q || [];
+
+            if (typeof namespace === "string") {
+              cal.ns[namespace] = cal.ns[namespace] || api;
+              p(cal.ns[namespace], ar);
+              p(cal, ["initNamespace", namespace]);
+            } else {
+              p(cal, ar);
+            }
+
+            return;
+          }
+
+          p(cal, ar);
+        };
+      })(window, "https://app.cal.com/embed/embed.js", "init");
+
+      Cal("init", "tech-help", {
+        origin: "https://app.cal.com"
       });
 
-      window.Cal.config = window.Cal.config || {};
-      window.Cal.config.forwardQueryParams = true;
+      Cal.config = Cal.config || {};
+      Cal.config.forwardQueryParams = true;
 
-      window.Cal.ns["tech-help"]("floatingButton", {
+      Cal.ns["tech-help"]("floatingButton", {
         calLink: "alyssa-gable/tech-help",
         config: {
           layout: "month_view",
-          useSlotsViewOnSmallScreen: "true",
+          useSlotsViewOnSmallScreen: "true"
         },
         buttonText: "Need some Tech Help?",
         buttonColor: "#ff0000",
-        buttonTextColor: "#ffffff",
+        buttonTextColor: "#ffffff"
       });
 
-      window.Cal.ns["tech-help"]("ui", {
+      Cal.ns["tech-help"]("ui", {
         hideEventTypeDetails: false,
-        layout: "month_view",
+        layout: "month_view"
       });
-    };
+    `;
 
     document.head.appendChild(script);
 
